fc706628eb2b9f3649db56f78c25072fb97132f3
Mauricio Zurita-Cortes <mzuritacortes@stu.jsu.edu>
2024-01-24 09:36:52 -0600

    Initial Commit

---
+++ src/mcis/jsu/edu/Main.java
@@ -0,0 +1,12 @@
+
+package mcis.jsu.edu;
+
+/**
+ *
+ * @author mauri
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(TestClass.testMethod1());
+    }
+}
---
+++ src/mcis/jsu/edu/TestClass.java
@@ -0,0 +1,12 @@
+
+package mcis.jsu.edu;
+
+/**
+ *
+ * @author mauri
+ */
+public class TestClass {
+   public static String testMethod1() {
+        return "Hello, World!";
+    }
+}
